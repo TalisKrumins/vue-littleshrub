0c291921a4ed4bc35b4ab1ae982731f1c5fc12fb
TalisKrumins <taliskrumins1988@gmail.com>
2023-03-10 16:26:20 +1000

    updatred css

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,24 @@
 <template>
-  <nav-bar>
-    <ul>
-      <li v-for="(children, parent) in menu" :key="parent" class="parent-item">
-        <span class="parent-link">{{ parent }}</span>
-        <ul>
-          <li v-for="child in children" :key="child.name">
-            <router-link :to="child.path" class="child-link">{{
-              child.name
-            }}</router-link>
-          </li>
-        </ul>
-      </li>
-    </ul>
-  </nav-bar>
+  <div>
+    <nav class="navbar navbar-expand-md navbar-dark bg-dark navbar-custom">
+      <ul>
+        <li
+          v-for="(children, parent) in menu"
+          :key="parent"
+          class="parent-item"
+        >
+          <span class="parent-link">{{ parent }}</span>
+          <ul>
+            <li v-for="child in children" :key="child.name">
+              <router-link :to="child.path" class="child-link">{{
+                child.name
+              }}</router-link>
+            </li>
+          </ul>
+        </li>
+      </ul>
+    </nav>
+  </div>
   <router-view />
 </template>
 
@@ -42,47 +48,47 @@ export default {
   color: #2c3e50;
 }
 
-<style > .nav-bar {
+<style > .navbar-custom {
   background-color: #f8f8f8;
   border-bottom: 1px solid #e7e7e7;
   margin-bottom: 20px;
 }
-.nav-bar .navbar-brand {
+.navbar-custom .navbar-brand {
   color: #777;
 }
-.nav-bar .navbar-brand:hover,
-.nav-bar .navbar-brand:focus {
+.navbar-custom .navbar-brand:hover,
+.navbar-custom .navbar-brand:focus {
   color: #5e5e5e;
 }
-.nav-bar .navbar-nav > li > a {
+.navbar-custom .navbar-nav > li > a {
   color: #777;
 }
-.nav-bar .navbar-nav > li > a:hover,
-.nav-bar .navbar-nav > li > a:focus {
+.navbar-custom .navbar-nav > li > a:hover,
+.navbar-custom .navbar-nav > li > a:focus {
   color: #333;
   background-color: transparent;
 }
-.nav-bar .navbar-nav > .active > a,
-.nav-bar .navbar-nav > .active > a:hover,
-.nav-bar .navbar-nav > .active > a:focus {
+.navbar-custom .navbar-nav > .active > a,
+.navbar-custom .navbar-nav > .active > a:hover,
+.navbar-custom .navbar-nav > .active > a:focus {
   color: #555;
   background-color: #e7e7e7;
 }
-.nav-bar .navbar-nav > .open > a,
-.nav-bar .navbar-nav > .open > a:hover,
-.nav-bar .navbar-nav > .open > a:focus {
+.navbar-custom .navbar-nav > .open > a,
+.navbar-custom .navbar-nav > .open > a:hover,
+.navbar-custom .navbar-nav > .open > a:focus {
   color: #555;
   background-color: #e7e7e7;
 }
-.nav-bar .navbar-nav > .dropdown > a:hover .caret {
+.navbar-custom .navbar-nav > .dropdown > a:hover .caret {
   border-top-color: #777;
   border-bottom-color: #777;
 }
-.nav-bar .navbar-nav > .dropdown > a .caret {
+.navbar-custom .navbar-nav > .dropdown > a .caret {
   border-top-color: #555;
   border-bottom-color: #555;
 }
-.nav-bar .navbar-nav .open .dropdown-menu {
+.navbar-custom .navbar-nav .open .dropdown-menu {
   background-color: #e7e7e7;
   border-radius: 0;
   border: none;
@@ -90,18 +96,19 @@ export default {
   margin-top: 2px;
   width: auto;
 }
-.nav-bar .navbar-nav .open .dropdown-menu > li > a {
+.navbar-custom .navbar-nav .open .dropdown-menu > li > a {
   color: #777;
   padding: 5px 15px;
 }
-.nav-bar .navbar-nav .open .dropdown-menu > li > a:hover,
-.nav-bar .navbar-nav .open .dropdown-menu > li > a:focus {
+.navbar-custom .navbar-nav .open .dropdown-menu > li > a:hover,
+.navbar-custom .navbar-nav .open .dropdown-menu > li > a:focus {
   color: #333;
   background-color: transparent;
 }
-.nav-bar .navbar-nav .open .dropdown-menu > .active > a,
-.nav-bar .navbar-nav .open .dropdown-menu > .active > a:hover,
-.nav-bar .navbar-nav .open .dropdown-menu > .active > a:focus {
+.navbar-custom .navbar-nav .open .dropdown-menu > .active > a,
+.navbar-custom .navbar-nav .open .dropdown-menu > .active > a:hover,
+.navbar-custom .navbar-nav .open .dropdown-menu > .active > a:focus {
   color: #555;
+  background-color: #e7e7e7;
 }
 </style>
